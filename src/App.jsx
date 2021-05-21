e3241a9801c1041f3a4ed83a9bbfbb65801c0844
WolfCanCode <truonglnse@gmail.com>
2021-05-21 13:33:52 +0700

    final style commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
-import React, { useEffect, useState } from "react"
-import { createApi } from "unsplash-js"
-import { useHistory, useLocation } from "react-router-dom"
-import _ from "lodash"
+import React, { useEffect, useState } from 'react'
+import { createApi } from 'unsplash-js'
+import { useHistory, useLocation } from 'react-router-dom'
+import _ from 'lodash'
 
-import "./App.css"
-import { Col, Empty, Pagination, Row, Space, Spin } from "antd"
+import './App.css'
+import { Col, Empty, Pagination, Row, Space, Spin } from 'antd'
 
 function App() {
   const history = useHistory()
 
-  const [search, setSearch] = useState(useQuery().get("search") || "")
+  const [search, setSearch] = useState(useQuery().get('search') || '')
   const [total, setTotal] = useState(0)
-  const [page, setPage] = useState(parseInt(useQuery().get("page")) || 1)
+  const [page, setPage] = useState(parseInt(useQuery().get('page')) || 1)
   const [loading, setLoading] = useState(false)
   const [imageList, setImageList] = useState([])
 
   const unsplash = createApi({
     accessKey: import.meta.env.VITE_UNS_KEY,
-    headers: { "X-Custom-Header": "foo" },
+    headers: { 'X-Custom-Header': 'foo' },
   })
 
   useEffect(() => {
     //run when load page
-    window.document.getElementById("search").value = search
+    window.document.getElementById('search').value = search
     doSearch(search)
   }, [])
 
@@ -34,7 +34,7 @@ function App() {
     return new URLSearchParams(useLocation().search)
   }
 
-  const doSearch = _.debounce(async (searchValue) => {
+  const doSearch = async (searchValue) => {
     setLoading(true)
     if (searchValue !== search) {
       setPage(1)
@@ -58,53 +58,53 @@ function App() {
               bio: result.user.bio,
               thumb: result.urls.thumb,
               link: result.links.html,
-            }))
+            })),
           )
         })
     }
-  }, 500)
+  }
 
   return (
     <div className="App">
-      <div className={"bg-container"}>
+      <div className={'bg-container'}>
         <h1>SWAT Challenge</h1>
-        <div className={"search-bar-container"}>
+        <div className={'search-bar-container'}>
           <Row>
             <Col xs={{ span: 18, offset: 3 }} lg={{ span: 8, offset: 8 }}>
-              <div className={"search-bar"}>
+              <div className={'search-bar'}>
                 <input
-                  id={"search"}
-                  placeholder={"Type something to search for image"}
-                  className={"search-input"}
-                  onChange={(e) => doSearch(e.target.value)}
+                  id={'search'}
+                  placeholder={'Type something to search for image'}
+                  className={'search-input'}
+                  onChange={_.debounce((e) => doSearch(e.target.value), 800)}
                 />
               </div>
             </Col>
           </Row>
         </div>
       </div>
-      <Space id={"image-list"} direction="vertical" className={"img-list-container"}>
+      <Space id={'image-list'} direction="vertical" className={'img-list-container'}>
         <Spin spinning={loading && search}>
           <Row gutter={16}>
             {search && imageList && imageList.length ? (
               imageList.map((imgItem) => (
                 <Col key={imgItem.link} xs={{ span: 12 }} lg={{ span: 6 }}>
                   <div
-                    className={"img-item"}
+                    className={'img-item'}
                     style={{
                       backgroundImage: `linear-gradient(to bottom,rgba(0,0,0,0),rgba(0,0,0,.4)),url('${imgItem.thumb}')`,
                     }}
                     onClick={() => window.open(imgItem.link)}
                   >
-                    <div className={"img-info"}>
+                    <div className={'img-info'}>
                       <p>{imgItem.name}</p>
-                      <p>{imgItem.bio || "..."}</p>
+                      <p>{imgItem.bio || '...'}</p>
                     </div>
                   </div>
                 </Col>
               ))
             ) : (
-              <div className={"empty-container"}>
+              <div className={'empty-container'}>
                 <Empty />
               </div>
             )}
@@ -113,18 +113,18 @@ function App() {
         {search && total !== 0 ? (
           <Pagination
             onChange={(page) => {
-              window.document.getElementById("image-list").scrollTop = 0
+              window.document.getElementById('image-list').scrollTop = 0
               setPage(page)
             }}
             current={page || 1}
-            className={"pagination"}
+            className={'pagination'}
             defaultCurrent={8}
             total={total}
             showSizeChanger={false}
             responsive
           />
         ) : (
-          ""
+          ''
         )}
       </Space>
     </div>
--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,4 @@ export default defineConfig({
   resolve: {
     alias: [{ find: /^~/, replacement: '' }],
   },
-  base: '/swat-challenge/',
 })
